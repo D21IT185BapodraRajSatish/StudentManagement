de0174e71be1f5c7f79256cc7f43ddaa1e65353e
D21IT185BapodraRajSatish <d21it185@charusat.edu.in>
2024-11-06 00:25:32 +0530

    add student list

--- internal/storage/storage.go
+++ internal/storage/storage.go
@@ -2,9 +2,8 @@ package storage
 
 import "github.com/D21IT185BapodraRajSatish/StudentAPI/internal/types"
 
-
-
 type Storage interface {
 	CreateStudent(name string, email string, age int) (int64, error)
-	GetStudentById(id int64) (types.Student,error)
+	GetStudentById(id int64) (types.Student, error)
+	GetStudents() ([]types.Student, error)
 }
